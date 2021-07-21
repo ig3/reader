d28026fc060bf309e0db33f4a6a3f54f29473a56
Ian Goodacre <Ian.Goodacre@entrain.co.nz>
2021-07-21 14:32:47 +1200

    Revise davereader.js per eslint/eslint-config-entrain

--- examples/feedFiler/filer.js
+++ examples/feedFiler/filer.js
@@ -1,60 +1,59 @@
-const reader = require ("davereader");
-const utils = require ("daveutils");
-const fs = require ("fs");
+const reader = require('davereader');
+const utils = require('daveutils');
+const fs = require('fs');
 
-const readerDataFolder = "reader_data/";
-const myOutputFolder = "myOutputFolder/";
+const readerDataFolder = 'reader_data/';
+const myOutputFolder = 'myOutputFolder/';
 
-var config = {
-	flHttpEnabled: false,
-	flWebSocketEnabled: false,
-	
-	listsFolder: readerDataFolder + "lists/",
-	riversFolder: readerDataFolder + "rivers/",
-	podcastsFolder: readerDataFolder + "podcasts/", 
-	dataFolder: readerDataFolder + "data/",
-	
-	addToRiverCallbacksFolder: readerDataFolder + "callbacks/addToRiver/",
-	buildRiverCallbacksFolder: readerDataFolder + "callbacks/buildRiver/",
-	
-	flRequestCloudNotify: false, 
-	flDownloadPodcasts: false
-	};
+const config = {
+  flHttpEnabled: false,
+  flWebSocketEnabled: false,
 
-var feeds = [
-	"http://scripting.com/rss.xml",
-	"http://status.aws.amazon.com/rss/ec2-us-east-1.rss",
-	"http://www.aclu.org/blog/feed/",
-	"http://www.bart.gov/news/rss/rss.xml",
-	"http://www.memeorandum.com/feed.xml",
-	"http://code4lib.org/node/feed",
-	"http://www.nytimes.com/pages/technology/index.html?partner=rssnyt",
-	"http://hn.geekity.com/newstories.xml",
-	"http://www.npr.org/rss/rss.php?id=1045"
-	];
-function writeFeedsList (callback) { 
-	var f = readerDataFolder + "lists/feeds.json", jsontext = utils.jsonStringify (feeds);
-	utils.sureFilePath (f, function () {
-		fs.writeFile (f, jsontext, function (err) {
-			if (err) {
-				console.log ("writeFeedsList: err.message == " + err.message);
-				}
-			else {
-				callback ();
-				}
-			});
-		});
-	}
+  listsFolder: readerDataFolder + 'lists/',
+  riversFolder: readerDataFolder + 'rivers/',
+  podcastsFolder: readerDataFolder + 'podcasts/',
+  dataFolder: readerDataFolder + 'data/',
+
+  addToRiverCallbacksFolder: readerDataFolder + 'callbacks/addToRiver/',
+  buildRiverCallbacksFolder: readerDataFolder + 'callbacks/buildRiver/',
+
+  flRequestCloudNotify: false,
+  flDownloadPodcasts: false
+};
+
+const feeds = [
+  'http://scripting.com/rss.xml',
+  'http://status.aws.amazon.com/rss/ec2-us-east-1.rss',
+  'http://www.aclu.org/blog/feed/',
+  'http://www.bart.gov/news/rss/rss.xml',
+  'http://www.memeorandum.com/feed.xml',
+  'http://code4lib.org/node/feed',
+  'http://www.nytimes.com/pages/technology/index.html?partner=rssnyt',
+  'http://hn.geekity.com/newstories.xml',
+  'http://www.npr.org/rss/rss.php?id=1045'
+];
+function writeFeedsList (callback) {
+  const f = readerDataFolder + 'lists/feeds.json'; const jsontext = utils.jsonStringify(feeds);
+  utils.sureFilePath(f, function () {
+    fs.writeFile(f, jsontext, function (err) {
+      if (err) {
+        console.log('writeFeedsList: err.message == ' + err.message);
+      } else {
+        callback();
+      }
+    });
+  });
+}
 
 function startup () {
-	config.newItemCallback = function (urlfeed, itemFromParser, item) { //called for each new item
-		var f = myOutputFolder + utils.getDatePath () + utils.padWithZeros (item.id, 4) + ".json";
-		utils.sureFilePath (f, function () {
-			fs.writeFile (f, utils.jsonStringify (item))
-			});
-		};
-	writeFeedsList (function () {
-		reader.init (config);
-		});
-	}
-startup ();
+  config.newItemCallback = function (urlfeed, itemFromParser, item) { // called for each new item
+    const f = myOutputFolder + utils.getDatePath() + utils.padWithZeros(item.id, 4) + '.json';
+    utils.sureFilePath(f, function () {
+      fs.writeFile(f, utils.jsonStringify(item));
+    });
+  };
+  writeFeedsList(function () {
+    reader.init(config);
+  });
+}
+startup();
--- examples/river5/river5.js
+++ examples/river5/river5.js
@@ -1,20 +1,19 @@
-var reader = require ("davereader"); 
-var fs = require ("fs");
+const reader = require('davereader');
+const fs = require('fs');
 function readConfig (callback) {
-	fs.readFile ("config.json", function (err, data) {
-		var myConfig = new Object ();
-		if (!err) {
-			try {
-				myConfig = JSON.parse (data.toString ());
-				}
-			catch (err) {
-				console.log ("readConfig: err == " + err.message);
-				}
-			}
-		callback (myConfig);
-		});
-	}
-readConfig (function (myConfig) {
-	reader.init (myConfig, function () {
-		});
-	});
+  fs.readFile('config.json', function (err, data) {
+    let myConfig = new Object();
+    if (!err) {
+      try {
+        myConfig = JSON.parse(data.toString());
+      } catch (err) {
+        console.log('readConfig: err == ' + err.message);
+      }
+    }
+    callback(myConfig);
+  });
+}
+readConfig(function (myConfig) {
+  reader.init(myConfig, function () {
+  });
+});
